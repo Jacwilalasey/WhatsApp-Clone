8b140eeb19f1eea707412ae3acedd1beebe939cb
Jacob Seymour <jacobwseymour@gmail.com>
2023-03-21 12:09:11 +0000

    adding text data json

---
+++ src/Data.js
@@ -0,0 +1,74 @@
+export const contactList = [
+    {
+      id: 1,
+      name: "Jacob Seymour",
+      profilePic: "/profile/profpic.png",
+      lastText: "Hello, how's it going?",
+      lastTextTime: "13:33 PM",
+    },
+    {
+      id: 2,
+      name: "Pat K",
+      profilePic: "/profile/user1.jpg",
+      lastText: `What's going on bro, we going out?`,
+      lastTextTime: "21:45 PM",
+    },
+    {
+      id: 3,
+      name: "Laura C",
+      profilePic: "/profile/user2.png",
+      lastText: "Te amo 💕",
+      lastTextTime: "12:30 AM",
+    },
+    {
+      id: 4,
+      name: "Mum",
+      profilePic: "/profile/user3.jpeg",
+      lastText: "Dad says hello!",
+      lastTextTime: "10:15 AM",
+    },
+  ];
+  export const messagesList = [
+    {
+      id: 1,
+      messageType: "TEXT",
+      text: "hello",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 2,
+      messageType: "TEXT",
+      text: "What's up?",
+      senderID: 1,
+      addedOn: "12:01 PM",
+    },
+    {
+      id: 3,
+      messageType: "TEXT",
+      text: "All Good, What about you?",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 4,
+      messageType: "TEXT",
+      text: "I'm good as well",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 5,
+      messageType: "TEXT",
+      text: "Great 😁",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 5,
+      messageType: "TEXT",
+      text: "Subscribed to Simplilarn",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+  ];
