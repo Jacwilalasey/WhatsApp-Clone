a3b04293c45d2906173b7331632290a11dee5f52
Jacob Seymour <jacobwseymour@gmail.com>
2023-03-22 18:49:24 +0000

    updated contact list section of app with contacts, search bar etc.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
-import { IoIosPeople } from 'react-icons/io';
+import { contactList } from "../Data";
+// import { IoIosPeople } from 'react-icons/io';
 
 // class Question extends React.Component {
 //   render() {
@@ -28,12 +28,99 @@ const ProfileImage = styled.img`
     border-radius: 50%;
 `;
 
+const SearchArea = styled.div`
+    display: flex;
+    background: #f6f6f6;
+    padding: 10px;
+`;
+
+export const SearchContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    background: white;
+    border-radius: 16px;
+    width: 100%;
+    padding: 20px 0;
+`;
+
+const SearchIcon = styled.img`
+    width: 28px;
+    height: 28px;
+    padding-left: 10px;
+`;
+
+export const SearchInput =styled.input`
+    width: 100%;
+    outline: none;
+    border: none;
+    padding-left: 15px;
+    font-size: 17px;
+    margin-left: 10px;
+`;
+
+const ContactItem =styled.div`
+    display: flex;
+    flex-direction : row;
+    border-bottom: 1px solid #f2f2f2;
+    background: white;
+    cursor: pointer;
+    padding: 15px 12px;
+`;
+
+const ProfileIcon = styled(ProfileImage)` 
+    width: 38px;
+    height: 38px;
+`;
+
+const ContactName= styled.span`
+    width: 100%;
+    font-size: 16px;
+    color:black;
+`;
+
+const MessageText = styled.span`
+    width: 20%;
+    font-size: 14px;
+    margin-top: 3px;
+    color: rgba (0,0,0,0.8);
+`;
+
+const ContactInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 0 19px;
+`;
+
+const ContactComponent =(props) => {
+    const { userData } = props;
+    return ( 
+        <ContactItem> 
+            <ProfileIcon src={userData.profilePic}/>
+            <ContactInfo>
+                <ContactName>{userData.name}</ContactName>
+                <MessageText>Hello</MessageText>
+            </ContactInfo>
+            <MessageText>10:04 PM</MessageText>
+        </ContactItem>
+    );
+};
+
 function Contacts() {
     return <Container>
         <ProfileDiv>
             <ProfileImage src="/profile/profpic.png"/>
         </ProfileDiv>
-        Contact List Component</Container>;
-}
+        <SearchArea>
+            <searchContainer>
+                <SearchIcon src={"search-icon.svg"}/>
+                <SearchInput placeholder="Search or start new chat"/>
+            </searchContainer>
+        </SearchArea>
+        {contactList.map((userData) => (
+            <ContactComponent userData={userData}/>
+        ))}
+        </Container>;
+};
 
 export default Contacts;
